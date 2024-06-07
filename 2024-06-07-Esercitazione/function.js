e291c34b0f269fd5efd19f60ef7abd00e42941a4
Lorenzolivacode <lorenzo.oliva.1308@gmail.com>
2024-06-07 18:10:19 +0200

    ho caricato ma non funziona

---
+++ 2024-06-07-Esercitazione/function.js
@@ -0,0 +1,37 @@
+export function QS (tag){
+    const QS = document.querySelector(tag);
+    return QS;
+}
+
+export function CE (ele){
+    const CE = document.createElement(ele);
+    return CE;
+};
+
+export function renderCard (filmObj){
+    const cardEl = CE('div');
+    cardEl.className = 'card';
+
+    const cardTitle = CE('h2');
+    cardTitle.className = 'card__title';
+    cardTitle.textContent = filmObj.title;
+
+    const cardImg = CE('img');
+    cardImg.className = 'card__img';
+    cardImg.src = `https://image.tmdb.org/t/p/w200/${filmObj.poster_path}`
+
+    const cardVote = CE('span');
+    cardVote.className = 'card__vote';
+    cardVote.textContent = filmObj.vote_average;
+
+    cardEl.append(cardTitle, cardImg, cardVote);
+}
+
+export function renderListCard (dataList){
+    const container = QS('.container');
+    container.innerHTML = "";
+
+    dataList.forEach((filmObj) => {
+        renderCard(filmObj);
+    });
+}
