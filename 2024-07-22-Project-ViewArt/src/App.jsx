44473d93030bcf60a30dc2be0f0152e0b732e48b
Lorenzolivacode <lorenzo.oliva.1308@gmail.com>
2024-07-23 10:50:46 +0200

    feat: tailwind css

--- 2024-07-22-Project-ViewArt/src/App.jsx
+++ 2024-07-22-Project-ViewArt/src/App.jsx
@@ -53,7 +53,7 @@ function App() {
 
           <div className="overflow-x-auto">
             <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
-              <thead className="ltr:text-left rtl:text-right">
+              <thead className="text-left">
                 <tr>
                   <th className="px-4 py-2"></th>
                   <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
