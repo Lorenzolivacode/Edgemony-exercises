0923b142323a9ba474de2f33bd8bbd58b05a3bcc
Lorenzolivacode <lorenzo.oliva.1308@gmail.com>
2024-09-05 21:52:34 +0200

    feat: add icon

--- 2024-09-02-ai-story-teller/src/(components)/(Molecules)/Header/Header.tsx
+++ 2024-09-02-ai-story-teller/src/(components)/(Molecules)/Header/Header.tsx
@@ -1,5 +1,6 @@
 import Button from "@/(components)/(Atoms)/Button/Button";
 import style from "./Header.module.scss";
+import icon from "./../../../../public/icons-book.png";
 
 interface HeaderProps {
   title: string;
@@ -7,6 +8,7 @@ interface HeaderProps {
 function Header({ title }: HeaderProps) {
   return (
     <header className={style.main}>
+      <img src="/icons-book.png" alt="Icon book" />
       <h1>{title}</h1>
       <Button label="Login" />
     </header>
--- 2024-09-02-ai-story-teller/src/pages/_app.tsx
+++ 2024-09-02-ai-story-teller/src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Il tuo titolo personalizzato</title>
+        <link rel="icon" href="/icons-book.png" />{" "}
+        {/* Modifica qui il percorso se necessario */}
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
